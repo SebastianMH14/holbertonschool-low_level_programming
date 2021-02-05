a4a477ed00bcf90d58bd4596b0ef35fa61b86709
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-05 03:43:50 +0000

    00..99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (a = 0; a < 100; a++)
 	{
-		putchar((a/10)+'0');
-		putchar((a%10)+'0');                 
-				
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+
 	if (a < 99)
-	{ 
+	{
 		putchar(',');
 		putchar(' ');
 	}
