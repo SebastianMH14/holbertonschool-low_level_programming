d2ffb3ce7fc128685dc406b190173d784f141b42
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-05 00:47:53 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,9 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("abcdefghijklmnopqrstuvwxyz");
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
