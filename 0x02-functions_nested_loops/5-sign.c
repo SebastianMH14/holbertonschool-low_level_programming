b08e238295e77a3a0e83b215edccc5063e07328a
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-09 19:32:55 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 
-
 /**
  * print_sign - check the code for Holberton School students.
  *@n: this is the check
@@ -11,14 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar ('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar ('0');
 		return (0);
 	}
 	else
 	{
+		_putchar ('-');
 		return (-1);
 	}
 }
