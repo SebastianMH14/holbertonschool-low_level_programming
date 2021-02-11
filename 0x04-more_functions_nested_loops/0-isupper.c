5a12ae83a3f1dc2c22ff8217636fa8b7cd32b3d8
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-11 23:37:01 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - check the code for Holberton School students.
+ *@c: this is the check
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int _isupper(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}}
