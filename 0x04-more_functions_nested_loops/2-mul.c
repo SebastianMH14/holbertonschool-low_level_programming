1de74a0bc193108d47214aba9436ff4e6853b274
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-12 00:38:42 +0000

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Adds two integers.
+ * @a: first int
+ * @b: second int
+ *
+ * Return: The result of the addition.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
