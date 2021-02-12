725d5d8810170c7d0f7e67ede64bcce52b8252e8
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-12 03:23:03 +0000

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,10 @@
  */
 void print_line(int n)
 {
-	int a;
-	
-	if (n <= 0)
-		_putchar('\n')
-		else
-		{
-			for (a = 1; a <= n; a++)
-			{
-				_putchar('_');
-			}
-		}
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
 	_putchar('\n');
 }
