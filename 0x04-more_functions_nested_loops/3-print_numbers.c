5fd159e51425a3b27b1fca198704c38149ab8a73
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-12 00:51:53 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints the digits (0 through 9).
+ *@a: the digit to detect
+ *
+ *Return: 0
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
