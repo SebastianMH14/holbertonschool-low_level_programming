40fb8fb79640cc0214205e3160d723ff01a5c6c0
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-17 03:03:14 +0000

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 
 /**
- * 
- * 
- * 
+ * puts2 - starting with the first character, followed by a new line.
+ * @str: value of str
+ *
+ * return: void
  */
 void puts2(char *str)
 {
-    int c = 0;
+	int c = 0;
 
 	for (c = 0; str[c] != '\0'; c += 2)
 	_putchar (str[c]);
