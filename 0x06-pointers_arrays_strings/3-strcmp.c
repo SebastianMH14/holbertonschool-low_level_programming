43e05d850e7dafce24e5e4a2f8ce5380b116f5d2
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-19 03:27:47 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
- int flag=0;  
-    while(*s1!='\0' && *s2!='\0')  
-    {  
-        if(*s1!=*s2)  
-        {  
-            flag=1;  
-        }  
-        s1++;  
-        s2++;  
-    }  
-      
-    if(flag==0)  
-    return 0;  
-    else  
-    return 1;  
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+	return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
