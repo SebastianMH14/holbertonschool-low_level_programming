49656d4e130c2c3ea6d445c0201ae75d758edda0
SebastianMH14 <sebasmh2002@hotmail.com>
2021-02-24 20:46:31 +0000

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@ void _puts_recursion(char *s)
 {
 	int i;
 
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar (s[i]);
-		i++;
-	}
-	_putchar(10);
+i = 0;
+if (s[i] != '\0')
+{
+_putchar(s[i]);
+_puts_recursion(&s[i+1]);
+}
+else    
+    _putchar(10);
 }
