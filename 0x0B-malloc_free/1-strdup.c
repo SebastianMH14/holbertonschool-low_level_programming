6fc406e7615fa5cccbbf3ec78b7d2eee55b66dde
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-02 17:07:14 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - hat returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: Return NULL if str = 0
+ */
+char *_strdup(char *str)
+{
+	int largo;
+	char *b;
+	char *c;
+
+	while (str[largo] != '\0')
+	{
+		largo++;
+	}
+	b = (char *)malloc(sizeof(char) * largo + 1);
+	if (b == NULL)
+	return ((char *)NULL);
+
+	c = b;
+	while (*str)
+	{
+		*c = *str;
+		c++;
+		str++;
+	}
+	*c = '\0';
+	return (b);
+}
