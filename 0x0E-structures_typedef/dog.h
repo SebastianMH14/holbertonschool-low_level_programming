27bd15567f4efb53218a830ee401332eb925c3b0
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-09 00:36:13 +0000

    readme and dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * struct dog - Short description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+}
+
+#endif
