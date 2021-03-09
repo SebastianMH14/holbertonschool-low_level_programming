9787552b8c42911c56dadbc7ff952de7adffdd70
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-09 00:03:31 +0000

    2main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints the name of the file it was compiled
+ *
+ *Return: 0
+ */
 
 int main()
 {
-	printf("2-main.c\n");
+	printf("%s\n",__FILE__);
 	return (0);
 }
