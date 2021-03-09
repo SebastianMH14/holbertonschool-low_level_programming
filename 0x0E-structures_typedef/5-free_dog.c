30d8e15e90fb8e18d9003025fb196c109669d2a8
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-09 04:40:29 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees dogs
+ *@d: free dogs
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
