db9d6af900dc007b13792f2abb0a6d4ddf791917
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-09 03:45:28 +0000

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,8 @@
  */
 void print_dog(struct dog *d)
 {
+	if(d)
+	{
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name);
@@ -25,5 +27,7 @@ void print_dog(struct dog *d)
 		printf("Owner: %s\n", d->owner);
 	}
 	else
+	{
 	printf("Owner: (nill)\n");
+	}
 }
