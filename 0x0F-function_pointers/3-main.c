3965d83b83b38365e2c30194c2e977c9cf2f7c43
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-11 04:15:04 +0000

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,6 +23,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
+	if ((*argv[2] == '%' || *argv[2] == '/') && n2 == 0)
+{
+ printf("Error/n");
+ exit(100);
+}
 	resultado = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", resultado);
 	return (0);
