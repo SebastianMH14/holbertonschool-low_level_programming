aa57149893879613dcca7cfdd3ed6f27520ad775
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-11 04:23:01 +0000

    fix calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,11 +23,6 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if (*argv[2] == '%' || *argv[2] == '/')
-{
- printf("Error/n");
- exit(100);
-}
 	resultado = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", resultado);
 	return (0);
