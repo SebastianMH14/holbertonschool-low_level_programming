2f47fed88f06d945cac75119cac86708121d4cb2
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-19 20:19:45 +0000

    header update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H_
 #include <stdio.h>
 #include <stdlib.h>
+ #include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,6 +25,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(char *s);
 
 
 #endif
