a80eaaa8a9fc064acddd99fb56b21f7cdf288ef7
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-19 18:56:21 +0000

    fix task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,18 @@
 
 /**
  *print_list -  function that prints all the elements of a list_t list.
+ *@h: head
  *
- *
- *
+ *Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	int j;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
 
 	for (j = 0; h != NULL; j++)
 	{
