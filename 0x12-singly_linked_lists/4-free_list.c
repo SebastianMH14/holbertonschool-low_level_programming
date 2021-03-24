9b55e92177fc7656d45a9d4c40551b7a6b20daa8
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-24 16:09:42 +0000

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list - function that frees a list_t list
+ *@head: list
+ *Return: void
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+		free(head->str);
+		free(head);
+	}
+}
