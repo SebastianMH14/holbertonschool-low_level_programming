76b08505b7378b5951a564c2a94223c403df3534
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-29 18:38:12 -0500

    fix task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (text_content == NULL)
 	{
-		return (-1);
+		return (1);
 	}
 	if (fd == -1)
 	{
