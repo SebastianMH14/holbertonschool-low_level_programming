0cfd4ea04aa6764baf11d90e53aed8f6e68011e0
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-28 19:38:13 -0500

    fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_table_t * ) * new_table->size);
+	new_table->array = malloc(sizeof(hash_table_t *) * new_table->size);
 	if (new_table->array == NULL)
 	{
 		return (NULL);
