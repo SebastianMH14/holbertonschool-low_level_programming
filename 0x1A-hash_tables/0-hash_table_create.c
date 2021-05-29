42968abe13dc4b70245b3d41b006eab5ad000c69
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-28 19:37:16 -0500

    fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,8 +17,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	new_table->size;
-	new_table->array = malloc(sizeof(hash_table_t *) * new_table->size);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_table_t * ) * new_table->size);
 	if (new_table->array == NULL)
 	{
 		return (NULL);
