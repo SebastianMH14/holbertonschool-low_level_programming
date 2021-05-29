ee8644e9c15f16a37f501da5735133add717a58a
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-28 21:44:57 -0500

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - function that deletes a hash table.
+ *@ht: is the hash table
+ *
+ *Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *aux = NULL;
+
+	while (i < ht->size)
+	{
+		while (ht->array[i])
+		{
+			aux = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = aux;
+		}
+		i++;
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
