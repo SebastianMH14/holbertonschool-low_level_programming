384445471d85858d2b69b4bca14a574abdc6756b
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-28 21:08:11 -0500

    fix 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[idx] == NULL)
 	{
-		ht->array[idx] == new_node;
+		ht->array[idx] = new_node;
 	}
 	else
 	{
